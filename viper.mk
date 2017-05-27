e49e7b3dabd7f42c2a3b9b850cc71996063f2a09
EsromG5 <esromgalaxy5@gmail.com>
2017-05-27 00:56:15 +0000

    Remove this for now

--- viper.mk
+++ viper.mk
@@ -11,11 +11,3 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_RELEASE_NAME := j5ltexx
 TARGET_DEVICE := j5lte
 
-# Support Poison
-PURE_POISON=true
-
-## OTAUpdate
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ota.romname=$(PRODUCT_NAME) \
-    ro.ota.version=$(shell date +%Y%m%d) \
-ro.ota.manifest=https://raw.githubusercontent.com/Viper0S/android_vendor_ViperOTA/n-mr1/$(PRODUCT_NAME).xml
